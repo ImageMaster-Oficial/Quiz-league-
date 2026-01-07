2a9afed2127d1924579df6597f537ccf4840844b
ImageMaster <ianfelixbelial@gmail.com>
2026-01-07 12:55:30 -0300

    Update and rename perguntas.js to pergunta.js

---
+++ pergunta.js
@@ -0,0 +1,18 @@
+// pergunta.js (Cole exatamente isso)
+export const bancoPerguntas = [
+    {
+        pergunta: "Qual campeão usa um poste como arma?",
+        opcoes: ["Darius", "Jax", "Garen", "Sion"],
+        correta: 1
+    },
+    {
+        pergunta: "Qual a função principal do Smite (Golpear)?",
+        opcoes: ["Curar aliados", "Abater monstros/tropas", "Dar dash", "Revelar mapa"],
+        correta: 1
+    },
+    {
+        pergunta: "Quantas rotas existem em Summoner's Rift?",
+        opcoes: ["1", "2", "3", "4"],
+        correta: 2
+    }
+];
